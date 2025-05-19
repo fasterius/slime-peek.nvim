3adaaf39292a155eeb6759b4dc515a4cff06c899
Erik Fasterius <erik.fasterius@outlook.com>
2025-05-19 08:00:24 +0200

    Improve comments; remove `set_cursor_position`

--- lua/slime-peek.lua
+++ lua/slime-peek.lua
@@ -10,18 +10,20 @@ function M.setup(opts)
     M.opts = vim.tbl_extend("force", M.opts, opts or {})
 end
 
--- Internal function for handling errors with a message
+--- Error handling
+-- Handle plugin errors by printing an error message and returning `nil` so that
+-- execution can gracefully complete.
+-- @param message the error message
+-- @return nil
 local function raise_error(message)
     vim.notify("Error: " .. message, vim.log.levels.ERROR)
     return nil
 end
 
--- Internal readability-improving function for setting the cursor position
-local function set_cursor_position(position)
-    vim.api.nvim_win_set_cursor(0, position)
-end
-
--- Internal function to get the language of a code chunk
+--- Get chunk language
+-- Check if cursor is inside a code chunk as well as parses and returns the
+-- language when this is the case.
+-- @return language
 local function get_chunk_language()
     -- Find a chunk start (header) backwards from the cursor position; if it
     -- can't be found the cursor is outside a chunk at the beginning of the file
@@ -55,20 +57,22 @@ local function get_chunk_language()
     end
 end
 
--- Local function to get the language from the Quarto YAML header
+--- Get YAML header language
+-- Check that the YAML header exists, is properly formatted and contains a
+-- language specification; return the language if this is the case.
+-- @return language
 local function get_yaml_language()
-    -- Store the current cursor position
+    -- Store the current cursor position and set the cursor position to the
+    -- beginning of the file, so that we can search for the YAML header
     local original_cursor_position = vim.api.nvim_win_get_cursor(0)
+    vim.api.nvim_win_set_cursor(0, { 1, 0 })
 
-    -- Set cursor position to beginning of file
-    set_cursor_position({ 1, 0 })
-
-    -- Get the ending line number of the YAML header
+    -- Get the line number for the end of the YAML header
     local yaml_end_line = vim.fn.search("^---$", "nW")
 
     -- Abort if no YAML header found
     if yaml_end_line == 0 then
-        set_cursor_position(original_cursor_position)
+        vim.api.nvim_win_set_cursor(0, original_cursor_position)
         return raise_error("YAML header not found; Quarto document is malformed")
     end
 
@@ -76,8 +80,9 @@ local function get_yaml_language()
     local pattern = "^knitr:\\|^jupyter:\\|^engine:"
     local line_number = vim.fn.search(pattern, "nW", yaml_end_line)
 
-    -- Reset cursor position
-    set_cursor_position(original_cursor_position)
+    -- Reset cursor position as all searches are now done, but before checking
+    -- if a match is found
+    vim.api.nvim_win_set_cursor(0, original_cursor_position)
 
     -- Raise error if no match is found
     if line_number == 0 then
@@ -110,7 +115,9 @@ local function get_yaml_language()
     end
 end
 
--- Internal function to get the language of the current file
+--- Get language for current file
+-- Check the current filetype and gets the corresponding language as appropriate
+-- @return language
 local function get_file_language()
     -- Access the filetype of the current buffer
     local filetype = vim.bo.filetype
@@ -131,10 +138,13 @@ local function get_file_language()
     end
 end
 
--- Internal function to get language-specific commands
+--- Handle R-specific commands
+-- Handle differing operation names between R and Python as well as whether the
+-- operation is a non-trivial function call with extra code.
+-- @param operation the operation to perform
+-- @param object the object to perform the operation on
+-- @return a string with the complete command
 local function get_r_command(operation, object)
-    -- Handle differing operation names between R and Python as well as whether
-    -- the operation is a non-trivial function call with extra code
     local extra = ""
     if operation == "dtypes" then
         operation = "sapply"
@@ -143,10 +153,14 @@ local function get_r_command(operation, object)
     return operation .. "(" .. object .. extra .. ")\\n"
 end
 
+--- Handle Python-specific commands
+-- Handle differing operation names between R and Python as well as whether the
+-- command pertains to an attribute (without parentheses) or a method (with
+-- parentheses)
+-- @param operation the operation to perform
+-- @object object the object to perform the operation on
+-- @return a string with the complete command
 local function get_python_command(operation, object)
-    -- Handle differing operation names between R and Python as well as whether
-    -- the command pertains to an attribute (without parentheses) or a method
-    -- (with parentheses)
     local parentheses = "()"
     if operation == "names" then
         operation = "columns.tolist"
@@ -159,49 +173,37 @@ local function get_python_command(operation, object)
     return object .. "." .. operation .. parentheses .. "\\n"
 end
 
--- Internal function to send a specific command to the REPL
+--- Send commands to the REPL
+-- Get the word under the cursor, the file language and the command and send it
+-- to the REPL. Does not return anything; errors are handled upstream.
+-- @param operation the operation to send
 local function send_command_to_repl(operation)
-    -- Get the word under the cursor
     local word_under_cursor = vim.fn.expand("<cword>")
-
-    -- Get the current language
     local language = get_file_language()
-
-    -- Get the language-appropriate command
     local command
     if language == "r" then
         command = get_r_command(operation, word_under_cursor)
     elseif language == "python" then
         command = get_python_command(operation, word_under_cursor)
     end
-
-    -- Send the command to the REPL
     if command then
         vim.cmd('SlimeSend0 "' .. command .. '"')
     end
 end
 
--- Function to print the head of the data frame under the cursor
+-- User-facing functions, one for each supported operation
 function M.peek_head()
     send_command_to_repl("head")
 end
-
--- Function to print the tail of the data frame under the cursor
 function M.peek_tail()
     send_command_to_repl("tail")
 end
-
--- Function to print the column names of the data frame under the cursor
 function M.peek_names()
     send_command_to_repl("names")
 end
-
--- Function to print the dimensions of the data frame under the cursor
 function M.peek_dimensions()
     send_command_to_repl("dim")
 end
-
--- Function to print the column types of the data frame under the cursor
 function M.peek_types()
     send_command_to_repl("dtypes")
 end
