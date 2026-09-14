56854466e32d31db10e5882d1e20ecc3edaaf5d8
Erik Fasterius <erik.fasterius@outlook.com>
2026-09-14 11:58:54 +0200

    Add tests for `util.lua`

---
+++ spec/util_spec.lua
@@ -0,0 +1,22 @@
+local util = require("slime_peek.util")
+
+describe("raise_error", function()
+    local notify_stub
+
+    before_each(function()
+        notify_stub = stub(vim, "notify")
+    end)
+    after_each(function()
+        notify_stub:revert()
+    end)
+
+    it("notifies with the given message, prefixed with 'Error: '", function()
+        util.raise_error("something went wrong")
+        ---@diagnostic disable-next-line: undefined-field
+        assert.stub(notify_stub).was_called_with("Error: something went wrong", vim.log.levels.ERROR)
+    end)
+
+    it("returns nil", function()
+        assert.is_nil(util.raise_error("something went wrong"))
+    end)
+end)
